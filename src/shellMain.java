f692f86cce9f62280875b1ae5ab206f77f280045
Danton <danton5276@gmail.com>
2014-02-13 11:56:17 -0500

    all working except cd

--- src/shellMain.java
+++ src/shellMain.java
@@ -1,16 +1,21 @@
 import java.io.*;
 import java.lang.*;
+import java.util.Scanner;
 
 public class shellMain
 {
 	public static void main(String[] args) throws java.io.IOException 
 	{
-		String commandLine;
+		String[] commandLine;
+		String delimiter = " ";
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		Process child;
 		while (true) 
 		{
 			System.out.print("mysh>");
-			commandLine = console.readLine();
+			
+			commandLine = (console.readLine()).split(delimiter);
+			
 			if (commandLine.equals(""))
 				continue;
 			else if(commandLine.equals("quit"))
@@ -19,13 +24,19 @@ public class shellMain
 			{
 				try
 				{
-				(Runtime.getRuntime().exec(commandLine)).waitFor();
+					child = Runtime.getRuntime().exec(commandLine);
+					Scanner scan = new Scanner(child.getInputStream());
+					
+					while(scan.hasNext())
+					{
+						System.out.println(scan.nextLine());
+					}
+					child.waitFor();
 				}
 				catch(InterruptedException e)
 				{
-					System.out.println("An interrupt exception was caught");
+					System.out.println("An interrupt exception was caught");	
 				}
-				
 			}
 		}
 	}
